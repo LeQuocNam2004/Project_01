791c38cbee74e6ddbafcf683fd089b7085e7fec2
LeQuocNam2004 <149886562+LeQuocNam2004@users.noreply.github.com>
2024-02-06 08:49:25 +0700

    Làm giao diện danh sách sản phẩm

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,4 +1,5 @@
 const dashboardRoutes = require("./dashboard.route");
+const productRoutes = require("./product.route");
 const systemConfig = require("../../config/system");
 
 module.exports = (app) => {
@@ -7,4 +8,6 @@ module.exports = (app) => {
 
   app.use(`${PATH_ADMIN}/dashboard`, dashboardRoutes);
 
+  app.use(`${PATH_ADMIN}/products`, productRoutes);
+  
 }
