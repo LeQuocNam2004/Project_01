6518169f23ae34de91db382bcd8937a0c95ff585
LeQuocNam2004 <149886562+LeQuocNam2004@users.noreply.github.com>
2024-02-07 23:33:51 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,6 +2,7 @@ const Product = require("../../models/product.model");
 
 // [GET] /
 module.exports.index = async(req, res) => {
+    // Sản phẩm nổi bật
     const productsFeatured = await Product.find({
         featured: "1",
         status: "active",
@@ -11,8 +12,21 @@ module.exports.index = async(req, res) => {
       for (const item of productsFeatured) {
         item.priceNew = item.price * (100 - item.discountPercentage)/100;
       }
+    // Hết Sản phẩm nổi bật
+
+    // Sản phẩm mới nhất
+    const productsNew = await Product.find({
+        status: "active",
+        deleted: false,
+    }).sort({ position: "desc" }).limit(6);
+
+    for (const item of productsNew) {
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+    }
+    // Hết Sản phẩm mới nhất
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productsFeatured: productsFeatured
+        productsFeatured: productsFeatured,
+        productsNew: productsNew
     });
 }
