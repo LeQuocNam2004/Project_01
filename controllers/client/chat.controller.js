a622926f8ac834adefc71ee6c6410fe00b26adbf
LeQuocNam2004 <149886562+LeQuocNam2004@users.noreply.github.com>
2024-03-01 14:04:09 +0700

    Tối ưu source code

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,6 +1,12 @@
 // [GET] /chat/
 module.exports.index = async (req, res) => {
-    res.render("client/pages/chat/index", {
-      pageTitle: "Chat",
-    });
+  // SocketIO
+  _io.on("connection", (socket) => {
+    console.log("Kết nối thành công!", socket.id)
+  });
+  // End SocketIO
+
+  res.render("client/pages/chat/index", {
+    pageTitle: "Chat",
+  });
 };
